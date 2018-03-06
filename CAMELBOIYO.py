44b1a431cf3b291331a18dd5527092cb4d834693
jo3l77 <34691008+jo3l77@users.noreply.github.com>
2018-03-06 14:47:58 -0500

    Add files via upload

---
+++ CAMELBOIYO.py
@@ -0,0 +1,14 @@
+# The CAMEL Game
+
+print("Welcome to Camel!")
+
+print("")
+print("You have stolen a camel from a family that lives in the beginning of the desert in order to make your way across the great Mojave Desert. \n")
+print("")
+print("The family has called the police and they have started trekking towards your position. \n")
+print("")
+print("Survive the desert trek and reach the other side while avoiding police!")
+
+Done = False
+while not_Done:
+    
